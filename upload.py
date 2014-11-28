c9c643ca2935feb8f2f1a98cfdee43531a322178
root <root@raspberrypi.(none)>
2014-11-28 07:23:01 +0000

    Updated Files

--- upload.py
+++ upload.py
@@ -3,21 +3,22 @@ import time
 
 
 
-os.chdir("/home/pi/Ali/tah-testbed/Vikas/Tahsketches/setAnalogHigh/")
+os.chdir("/home/pi/GitRepo/Tah_Testbed/Tahsketches/blink/")
 time.sleep(1)
 os.system("make")
-print "Compiled for Analog HIGH State"
+print "Compiled for LED BLINK"
 os.system("make upload")
-print "Uploaded Complete Analog HIGH"
-time.sleep(5)
-
-os.chdir("/home/pi/Ali/tah-testbed/Vikas/Tahsketches/setAnalogLow")
+print "Uploaded Complete "
 time.sleep(1)
-os.system("make")
-os.system("make upload")
-print "Upload Complete Analog LOW State"
 
-time.sleep(2)
+#os.chdir("/home/pi/GitRepo/tah-testbed/Vikas/Tahsketches/setAnalogLow")
+#time.sleep(1)
+#os.system("make")
+#os.system("make upload")
+#print "Upload Complete Analog LOW State"
+
+#time.sleep(1)
+
     
 print "END"
 
